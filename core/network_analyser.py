75f8125d6be929557edd0b72ff40f85c63302466
Talha Junaid <talhajunaidd@gmail.com>
2018-09-26 10:54:12 +0500

    completed rene thomas' algorithm

--- core/network_analyser.py
+++ core/network_analyser.py
@@ -33,16 +33,67 @@ class NetworkAnalyser:
     @staticmethod
     def get_state_graph(network: DiGraph, parameters):
         state_space = list(NetworkAnalyser.generate_state_space(network.nodes))
-
         state_graph = nx.DiGraph()
-        state_space_nodes = [''.join(map(str, entry.values())) for entry in state_space]
+        state_space_nodes = [utils.create_node_from_dict(entry) for entry in state_space]
         state_graph.add_nodes_from(state_space_nodes)
 
-        next_state = dict()
+        resources = NetworkAnalyser.calculate_resources(network, state_space)
+
+        k_states = NetworkAnalyser.calculate_k(resources, parameters)
+
+        next_states = NetworkAnalyser.calculate_next_states(k_states)
+
+        edges = NetworkAnalyser.generate_edges(next_states)
+        state_graph.add_edges_from(edges)
+        return state_graph
+
+    @staticmethod
+    def generate_edges(next_states):
+        edges = list()
+        for key, states in next_states.items():
+            previous_value = dict(key)
+            source = utils.create_node_from_dict(previous_value)
+            for state in states:
+                target = utils.create_node_from_dict(state)
+                edges.append((source, target))
+        return edges
+
+    @staticmethod
+    def calculate_next_states(k_states):
+        for state_key, state in k_states.items():
+            previous_entities = dict(state_key)
+            new_val = list()
+            temp_previous = previous_entities.copy()
+            for entity_key, previous_entity in previous_entities.items():
+                next_value = k_states[state_key][entity_key]
+
+                if next_value > previous_entity:
+                    previous_entities[entity_key] = previous_entity + 1
+                elif next_value < previous_entity:
+                    previous_entities[entity_key] = previous_entity - 1
+
+                if not (previous_entities in new_val) and not previous_entities == temp_previous:
+                    new_val.append(previous_entities.copy())
+                    previous_entities[entity_key] = temp_previous[entity_key]
+            k_states[state_key] = new_val
+        return k_states
+
+    @staticmethod
+    def calculate_k(resources, parameters):
+        for resource_key, entities in resources.items():
+            for entity_key, entity in entities.items():
+                matched_interaction = filter(lambda x: sorted(x['interaction']) == sorted(entity), parameters[entity_key])
+                first_interaction = next(matched_interaction)
+                entities[entity_key] = first_interaction['value']
+        return resources
+
+    @staticmethod
+    def calculate_resources(network, state_space):
+        resources = dict()
         for state in state_space:
             node = dict()
             for key in state:
-                resources = list()
+                entity_resources = list()
                 in_edges = network.in_edges(key, data=True)
                 for edge in in_edges:
                     weight = edge[2]['weight']
@@ -51,14 +102,8 @@ class NetworkAnalyser:
                     is_resource = (is_positive and value >= abs(weight)) or (
                         not is_positive and value < abs(weight))
                     if is_resource:
-                        resources.append(edge[0])
-                node[key] = resources
-            state_key = tuple(frozenset(state.items()))
-            next_state[state_key] = node
-
-        for key, state in next_state.items():
-            for entry_Key, entity in state.items():
-                matched_interaction = next(
-                    filter(lambda x: sorted(x['interaction']) == sorted(entity), parameters[entry_Key]))
-                state[entry_Key] = matched_interaction['value']
-        print(next_state)
+                        entity_resources.append(edge[0])
+                node[key] = entity_resources
+            state_key = tuple(state.items())
+            resources[state_key] = node
+        return resources
--- core/utils.py
+++ core/utils.py
@@ -3,3 +3,7 @@ import itertools
 
 def all_subsets(items):
     return itertools.chain(*map(lambda x: itertools.combinations(items, x), range(0, len(items) + 1)))
+
+
+def create_node_from_dict(item: dict):
+    return ''.join(map(str, item.values()))
